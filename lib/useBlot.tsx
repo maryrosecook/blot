3ab6be7f94734e6e24ca98ad770de176b2dea3da
Mary Rose Cook <mary@maryrosecook.com>
2023-10-27 22:29:10 -0700

    Refactor so you get back Editor from useBlot (formerly useEditor) and it's a real component not rendered JSX

--- lib/useBlot.tsx
+++ lib/useBlot.tsx
@@ -1,13 +1,7 @@
 import { useCallback, useRef } from 'react';
 import { Editor } from './Editor';
 
-export function useEditor({
-  width,
-  height,
-}: {
-  width: number;
-  height: number;
-}) {
+export function useBlot() {
   const editorRef = useRef<{ getImageData: () => void }>(null);
 
   const getImageData = useCallback(() => {
@@ -16,9 +10,10 @@ export function useEditor({
     }
   }, []);
 
-  const EditorComponent = (
-    <Editor width={width} height={height} ref={editorRef} />
-  );
-
-  return { getImageData, EditorComponent };
+  return {
+    getImageData,
+    Editor: ({ width, height }: { width: number; height: number }) => (
+      <Editor width={width} height={height} ref={editorRef} />
+    ),
+  };
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import { Blot } from '../lib/Blot';
+import { useEffect } from 'react';
+import { useBlot } from '../lib/useBlot';
 
 function App() {
+  const { getImageData, Editor } = useBlot();
+
+  useEffect(() => {
+    getImageData();
+  }, [getImageData]);
+
   return (
     <div className="m1 p1 border border-gray rounded inline-block">
-      <Blot />
+      <Editor width={200} height={200} />
     </div>
   );
 }
